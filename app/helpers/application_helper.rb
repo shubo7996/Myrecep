7e64d070e438e0fa2227952ca44ae63c282370b6
Subhamoy <Whatsupmofo420@gmail.com>
2017-05-30 11:41:42 +0530

    created form partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	
+	def gravatar_for(chef, option={ size: 80})
+		gravatar_id = Digest::MD5::hexdigest(chef.email.downcase)
+		size = option[:size]
+		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+		image_tag(gravatar_url, alt: chef.chefname, class: "gravatrar")
+	end
 end
